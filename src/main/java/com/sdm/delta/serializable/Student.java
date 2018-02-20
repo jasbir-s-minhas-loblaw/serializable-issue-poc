39c98bfde6381d93e5cf71ef8bbe5ef6b7f2f7de
Jasbir Minhas <jasbir.minhas@loblaw.ca>
2018-02-20 00:28:55 -0500

    Added System.exit(1) on purpose to see if it is picked up by SonarQube.

--- src/main/java/com/sdm/delta/serializable/Student.java
+++ src/main/java/com/sdm/delta/serializable/Student.java
@@ -1,6 +1,6 @@
 package com.sdm.delta.serializable;
 
-import java.io.Serializable;
+import java.io.BufferedReader;
 
 public class Student extends Person {
     public static final long serialVersionUID = 1234L;
@@ -9,6 +9,7 @@ public class Student extends Person {
 
     public Student(long studentId, String name, int age) {
         super();
+        System.exit(1);
         this.studentId = studentId;
         super.name = name;
         super.age = age;
@@ -17,6 +18,7 @@ public class Student extends Person {
 
     @Override
     public String toString() {
+        System.exit(1);
         return "Student{" +
                 "studentId=" + studentId +
                 ", name='" + name + '\'' +
